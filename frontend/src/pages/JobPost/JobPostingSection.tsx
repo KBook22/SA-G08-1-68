799aa1cb2ca9d1e2cc86be277d44d75bd82d2f0b
Netnaphat <Netnaphat.cha@gmail.com>
2025-08-24 14:38:17 +0700

    edit jobpost

--- frontend/src/pages/JobPost/JobPostingSection.tsx
+++ frontend/src/pages/JobPost/JobPostingSection.tsx
@@ -159,17 +159,7 @@ const JobDescriptionSection: React.FC = () => {
         style={{ marginTop: 16, width: "100%" }}
       />
 
-      {/* ปุ่มยืนยัน */}
-      <div className="submit-button-wrapper">
-        <Button
-          type="primary"
-          size="large"
-          className="submit-button"
-          htmlType="submit"
-        >
-          ยืนยัน
-        </Button>
-      </div>
+
     </div>
   );
 };
